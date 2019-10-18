8843e4d21ac3e5496adc8515e0ae04e4726ba2ae
amark02 <44906842+amark02@users.noreply.github.com>
2019-10-18 10:02:04 -0400

    Create trucks.py

---
+++ trucks.py
@@ -0,0 +1,18 @@
+class Incoming_trucks:
+    def __init__(self, clothing, electronics, automotive, home_kitchen, 
+    sports, tools, toys_games):  
+        self.clothing = clothing
+        self. electronics = electronics
+        self.automotive = automotive
+        self.home_kitchen = home_kitchen
+        self.sports = sports
+        self.tools = tools
+        self.toys_game = toys_games
+
+class Outgoing_trucks:
+    def __init__(self, short_distance, med_distance, long_distance):
+        self.short_distance = short_distance
+        self.med_distance = med_distance
+        self.long_distance = long_distance
+
+  
