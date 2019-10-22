1e652484b3334585351ec4c06c2614789e699605
simonflyworld <37817893+simonflyworld@users.noreply.github.com>
2019-10-22 11:23:02 -0400

    Create Incoming_List.py

---
+++ Incoming_List.py
@@ -0,0 +1,51 @@
+class Incoming_List:
+    def __init__(self, clothing, electronics, automotive, home_kitchen, 
+    sports, tools, toys_games):  
+    """ Add two numbers.
+
+    Arg:
+        clothing: An clothing item
+        electronics: An electronics item
+        automotive: An automotive item
+        home_kitchen: An home_kitchen item
+        sports: An sports item
+        tools: An tools item
+        toys_games: An toys_games item
+
+    Returns:
+        clothing: An clothing dict
+        electronics: An electronics dict
+        automotive: An automotive dict
+        home_kitchen: An home_kitchen dict
+        sports: An sports dict
+        tools: An tools dict
+        toys_games: An toys_games dict
+    """
+        clothing{} = self.clothing
+        electronics{} = self.electronics
+        automotive{} = self.automotive
+        home_kitchen{} = self.home_kitchen
+        sports{} = self.sports
+        tools{} = self.tools
+        toys_games{} = self.toys_games
+    
+    def clothing():
+        return clothing{}
+    
+    def electronics():
+        return electronics{}
+    
+    def automotive():
+        return automotive{}    
+    
+    def home_kitchen():
+        return home_kitchen{}
+    
+    def sports():
+        return sports{}
+    
+    def tools():
+        return tools{}
+    
+    def toys_games():
+        return toys_games{}
