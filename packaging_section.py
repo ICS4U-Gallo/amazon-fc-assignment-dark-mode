d3ffe1c21659154707df71df704fe224a2ce343e
amark02 <44906842+amark02@users.noreply.github.com>
2019-10-28 11:08:30 -0400

    Update packaging_section.py

--- packaging_section.py
+++ packaging_section.py
@@ -1,13 +1,31 @@
 class Package:
-    pass
+    """Package class
 
-    def __init__(self, item, small, medium, large):
+    Attributes:
+        item (str): the type of item being packaged
+        size (str): size of the package
+        address (str): where the package is being shipped 
+    """
+    def __init__(self, item: str, size: str, address: str):
+        """Create a package object
+
+        Args:
+            item: item being packaged
+            size: size of the package
+            address: where the package is being shipped
+        """
         self.item = item
-        self.small = small
-        self.medium = medium
-        self.large = large
+        self.size = size
+        self.address = address
+
+    def get_package(self): 
+        pass  
 
     def add(self, item):
+        item.oven = self.item
+        item.size = self.size
+        item.address = self.address
+
     
     
     
