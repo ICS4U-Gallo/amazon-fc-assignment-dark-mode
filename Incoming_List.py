0ec058dcd99c0ad32432d0d1e0baaa7eb32908cb
simonflyworld <37817893+simonflyworld@users.noreply.github.com>
2019-10-30 11:04:33 -0400

    Update Incoming_List.py

--- Incoming_List.py
+++ Incoming_List.py
@@ -1,51 +1,62 @@
 class Incoming_List:
+    List = {}
     def __init__(self, clothing, electronics, automotive, home_kitchen, 
     sports, tools, toys_games):  
-    """ Add two numbers.
+        """ Add two numbers.
 
-    Arg:
-        clothing: An clothing item
-        electronics: An electronics item
-        automotive: An automotive item
-        home_kitchen: An home_kitchen item
-        sports: An sports item
-        tools: An tools item
-        toys_games: An toys_games item
+        Arg:
+            clothing: An clothing item
+            electronics: An electronics item
+            automotive: An automotive item
+            home_kitchen: An home_kitchen item
+            sports: An sports item
+            tools: An tools item
+            toys_games: An toys_games item
 
-    Returns:
-        clothing: An clothing dict
-        electronics: An electronics dict
-        automotive: An automotive dict
-        home_kitchen: An home_kitchen dict
-        sports: An sports dict
-        tools: An tools dict
-        toys_games: An toys_games dict
-    """
-        clothing{} = self.clothing
-        electronics{} = self.electronics
-        automotive{} = self.automotive
-        home_kitchen{} = self.home_kitchen
-        sports{} = self.sports
-        tools{} = self.tools
-        toys_games{} = self.toys_games
-    
+        Returns:
+            clothing: An clothing dict
+            electronics: An electronics dict
+            automotive: An automotive dict
+            home_kitchen: An home_kitchen dict
+            sports: An sports dict
+            tools: An tools dict
+            toys_games: An toys_games dict
+        """
+
+        clothing = self.clothing
+        electronics = self.electronics
+        automotive = self.automotive
+        home_kitchen = self.home_kitchen
+        sports = self.sports
+        tools = self.tools
+        toys_games = self.toys_games
+        List = {"clothing": self.clothing, 
+                "electronics": self.electronics, 
+                "automotive": self.automotive, 
+                "home_kitchen": self.home_kitchen, 
+                "sports": self.sports, 
+                "tools": self.tools, 
+                "toys_games": self.toys_games, 
+                }
     def clothing():
-        return clothing{}
+        return List["clothing"]
     
     def electronics():
-        return electronics{}
+        return List["electronics"]
     
     def automotive():
-        return automotive{}    
+        return List["automotive"]  
     
     def home_kitchen():
-        return home_kitchen{}
+        return List["home_kitchen"]
     
     def sports():
-        return sports{}
+        return List["sports"]
     
     def tools():
-        return tools{}
+        return List["tools"]
     
     def toys_games():
-        return toys_games{}
+        return List["toys_games"]
+
+
