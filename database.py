f280e96b00c4e7473a406773a2c37d029410641f
terrykim11 <terry.kim20@ycdsbk12.ca>
2019-10-31 10:46:01 -0400

    awd

--- database.py
+++ database.py
@@ -8,6 +8,38 @@ class Item:
     self.size = size
     self.item_type = item_type
 
+with open("database_storage.pickle", "rb") as f:
+    database = pickle.load(f)
+
+for item in truck.clothing:
+    database["clothing"].append(item)
+for item in truck.electronics:
+    database["electronics"].append(item)
+for item in truck.automotive:
+    database["automotive"].append(item)
+for item in truck.home_kitchen:
+    database["home_kitchen"].append(item)
+for item in truck.sports:
+    database["sports"].append(item)
+for item in truck.tools:
+    database["tools"].append(item)
+for item in truck.toys_game:
+    database["toy_game"].append(item)
+
+with open("database_storage.pickle", "wb") as f:
+    pickle.dump(database, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+
+
+
+
+
+"""
+database = {"clothing":[], "electronics":[], "automotive":[], "home_kitchen":[],
+            "sports":[], "tools":[], "toy_game":[]}
+
 item1 = Item("Tee-Shirt", "small", "clothing")
 item2 = Item("Gucci Belt", "small", "clothing")
 item3 = Item("Iphone X", "small", "electronics")
@@ -23,48 +55,12 @@ item12 = Item("Power Drill", "medium", "tools")
 item13 = Item("Nintendo 64", "medium", "toy_games")
 item14 = Item("Cards", "small", "toy_games")
 
-#ruck = Incoming_trucks([item1, item2], [item3, item4], [item5, item6], [item7, item8],
-#             [item9, item10], [item11, item12], [item13, item14])
+ruck = Incoming_trucks([item1, item2], [item3, item4], [item5, item6], [item7, item8],
+             [item9, item10], [item11, item12], [item13, item14])
 
 ruck = Incoming_trucks([],[],[],[],[],[],[])
 
-
-
-with open("database_storage.pickle", "rb") as f:
-    database = pickle.load(f)
-
-
-for item in ruck.clothing:
-    database["clothing"].append(item)
-for item in ruck.electronics:
-    database["electronics"].append(item)
-for item in ruck.automotive:
-    database["automotive"].append(item)
-for item in ruck.home_kitchen:
-    database["home_kitchen"].append(item)
-for item in ruck.sports:
-    database["sports"].append(item)
-for item in ruck.tools:
-    database["tools"].append(item)
-for item in ruck.toys_game:
-    database["toy_game"].append(item)
-
-with open("database_storage.pickle", "wb") as f:
-    pickle.dump(database, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-
-
-
-print(database["clothing"][0].name)
-
-
-
 """
-database = {"clothing":[], "electronics":[], "automotive":[], "home_kitchen":[],
-            "sports":[], "tools":[], "toy_game":[]}
-"""
-
 
 
 
